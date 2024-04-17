b0678f0f66ae4c701154fa3a00e37cb484c62471
SarahOtmane <sarahotmane02@gmail.com>
2024-04-17 22:05:08 +0200

    modif nav dans Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,11 +6,13 @@ const Header = () =>{
         <header className='row alignitem_center'>
             <section className='row alignitem_center'>
                 <img src={logo} alt="RenoWear logo en noir" />
-                <nav className='row'>
-                    <a href="/">À propos</a>
-                    <a href="/">Nos artisans</a>
-                    <a href="/">Aide</a>
-                    <a href="/">Contact</a>
+                <nav>
+                    <ul className='row'>
+                        <li><a href="/">À propos</a></li>
+                        <li><a href="/">Nos artisans</a></li>
+                        <li><a href="/">Aide</a></li>
+                        <li><a href="/">Contact</a></li>
+                    </ul>
                 </nav>
             </section>
             <button>Commencer à réparer</button>
