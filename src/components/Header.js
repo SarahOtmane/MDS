ff82c9d16d32e418f5e81ba9b8662ae5b23d9969
SarahOtmane <sarahotmane02@gmail.com>
2024-04-17 21:43:36 +0200

    première version du header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,18 @@
-
+import logo from '../assets/pictures/logo_black.png';
 
 const Header = () =>{
-
-
-
+    
     return(
-        <></>
+        <header>
+            <img src={logo} alt="RenoWear logo en noir" />
+            <nav>
+                <a href="/">À propos</a>
+                <a href="/">Nos artisans</a>
+                <a href="/">Aide</a>
+                <a href="/">Contact</a>
+            </nav>
+            <button>Commencer à réparer</button>
+        </header>
     )
 }
 
