0ceda62dbf5ffe112756a8746bc2f18a45ec135a
SarahOtmane <sarahotmane02@gmail.com>
2024-04-17 22:41:11 +0200

    section1 de LandingPage

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,9 +1,25 @@
-
+import mockup from '../assets/pictures/landing_mockup.png';
 
 const LandingPage = () =>{
     return(
-        <main>
+        <main className="landing">
+            <section className="section1 backGris row">
+                <article>
+                    <h1 className='text_uppercase'>pourquoi jeter quand on peut réparer ?</h1>
+                    <p>Le concept de réparation et de consommation de vêtements durables</p>
+                    <p>Faites le tri dans vos habits.</p>
+                    <p className='text_bold'>Télécharger RenoWear</p>
+                    <div className='row'>
+                        <button>
 
+                        </button>
+                        <button>
+
+                        </button>
+                    </div>
+                </article>
+                <img src={mockup} alt='' />
+            </section>
         </main>
     )
 }
