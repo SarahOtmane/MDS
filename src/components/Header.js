c212efbb98805e270503950568069152e9396833
SarahOtmane <sarahotmane02@gmail.com>
2024-04-17 21:53:50 +0200

    ajout section dans Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,15 @@ const Header = () =>{
 
     return(
         <header className='row'>
-            <img src={logo} alt="RenoWear logo en noir" />
-            <nav className='row'>
-                <a href="/">À propos</a>
-                <a href="/">Nos artisans</a>
-                <a href="/">Aide</a>
-                <a href="/">Contact</a>
-            </nav>
+            <section className='row'>
+                <img src={logo} alt="RenoWear logo en noir" />
+                <nav className='row'>
+                    <a href="/">À propos</a>
+                    <a href="/">Nos artisans</a>
+                    <a href="/">Aide</a>
+                    <a href="/">Contact</a>
+                </nav>
+            </section>
             <button>Commencer à réparer</button>
         </header>
     )
