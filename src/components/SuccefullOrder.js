e3074c29adc080b5843219ad592e417f01f3d4df
SarahOtmane <sarahotmane02@gmail.com>
2024-04-27 22:54:51 +0200

    créer un composant pour la page commande réussie

---
+++ src/components/SuccefullOrder.js
@@ -0,0 +1,13 @@
+import Titre from "./Titre"
+
+
+
+const SuccefullOrder = () =>{
+    return(
+        <main>
+            <Titre title="Merci pour votre commande" lien="/succes-order" />
+        </main>
+    )
+}
+
+export default SuccefullOrder;
