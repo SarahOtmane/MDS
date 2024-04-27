9d32577969b90438426d444f5e5bede33653cf49
SarahOtmane <sarahotmane02@gmail.com>
2024-04-27 23:39:59 +0200

    crétion d'un fichier pour visualise les commandes effectue par un user

---
+++ src/components/Commandes.js
@@ -0,0 +1,14 @@
+import '../css/cart.css';
+import Titre from "./Titre";
+
+
+const Commandes = () =>{
+    return(
+        <main>
+            <Titre titre="Mon compte" lien="/my-account" classe="backRose" />
+            
+        </main>
+    )
+}
+
+export default Commandes;
