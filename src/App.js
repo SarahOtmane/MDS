60fd65cebd73ae3af5c537cbb7343cdb1227ce26
SarahOtmane <sarahotmane02@gmail.com>
2024-04-27 22:55:05 +0200

    import du composant SuccefullOrder dans App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import SignIn from "./components/SignIn";
 import ForgetMdp from "./components/ForgetMdp";
 import ResetMdp from "./components/ResetMdp";
 import CheckCart from "./components/CheckCart";
+import SuccefullOrder from "./components/SuccefullOrder";
 
 
 const App = () =>{
@@ -19,11 +20,15 @@ const App = () =>{
                 <Header />
                 <Routes>
                     <Route path="/landing" element={<LandingPage />} /> 
+
                     <Route path="/register" element={<SignUp />} />
                     <Route path="/login" element={<SignIn />} />
+
                     <Route path="/resetMdp" element={<ForgetMdp />} />
                     <Route path="/resetMdp/reset" element={<ResetMdp />} />
+                    
                     <Route path="/cart/check" element={<CheckCart />} />
+                    <Route path="/success-order" element={<SuccefullOrder />} />
                 </Routes> 
                 <Footer />
             </>
