b9e7f3436bb95918e5ff51b6edd394215703f9ab
SarahOtmane <sarahotmane02@gmail.com>
2024-04-27 21:26:12 +0200

    composant valider la commande

---
+++ src/components/CheckCart.js
@@ -0,0 +1,86 @@
+import { useNavigate } from 'react-router-dom';
+
+import '../css/cart.css'
+import Titre from './Titre';
+
+import product from '../assets/pictures/landingPage/product1.png';
+
+
+const CheckCart = () =>{
+    const navigate = useNavigate();
+
+    return(
+        <main>
+            <Titre titre="Valider votre panier" lien="/cart/check" />
+
+            <form className='row'>
+                <section className='livraison column'>
+                    <h2>Adresse de livraison</h2>
+
+                    <lable className="text_bold">Numéro de rue</lable>
+                    <input type="text" placeholder="30 Rue Solférino" />
+
+                    <lable className="text_bold">Ville</lable>
+                    <input type="text" placeholder="Boulogne-Billancourt" />
+
+                    <div className='row'>
+                        <div className='column'><lable className="text_bold">Code Postal</lable>
+                        <input type="text" placeholder="92100" /></div>
+
+                        <div className='column'><lable className="text_bold">Pays</lable>
+                        <input type="text" placeholder="France" /></div>
+                    </div>
+
+                    <lable className="text_bold">Email</lable>
+                    <input type="text" placeholder="test@gmail.com" />
+
+                    <div className='row'>
+                        <div className='column'><lable className="text_bold">Nom</lable>
+                        <input type="text" placeholder="Cruz" /></div>
+
+                        <div className='column'><lable className="text_bold">Prénom</lable>
+                        <input type="text" placeholder="Tom" /></div>
+                    </div>
+                </section>
+
+                <section className='commande' >
+                    <h2>Votre commande</h2>
+                    <article className='row alignitem_center'>
+                        <img src={product} alt='' />
+                        <img src={product} alt='' />
+
+                        <button onClick={navigate('/')} >Modifier</button>
+                    </article>
+
+                    <section className='recap'>
+                        <article className='row'>
+                            <span>Prix :</span>
+                            <span>75.00€</span>
+                        </article>
+
+                        <article className='row'>
+                            <span>Livraison :</span>
+                            <span>Offerte</span>
+                        </article>
+
+                        <article className='row'>
+                            <span>Taxes :</span>
+                            <span>3.00€</span>
+                        </article>
+
+                        <div></div>
+
+                        <article className='row'>
+                            <span>Total :</span>
+                            <span>78.00€</span>
+                        </article>
+                    </section>
+
+                    <button>Payer la commande</button>
+                </section>
+            </form>
+        </main>
+    )
+}
+
+export default CheckCart;
