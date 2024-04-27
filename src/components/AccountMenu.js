7cc543adf25e6e3a3ae5882be2c2c2eccbf9b2d3
SarahOtmane <sarahotmane02@gmail.com>
2024-04-27 23:47:22 +0200

    créa d'un composant menu avec tous les lien vers les différentes pages d'un compte

---
+++ src/components/AccountMenu.js
@@ -0,0 +1,16 @@
+
+
+
+const AccountMenu = () =>{
+    return(
+        <ul className="menu">
+            <li><a href='/'>Commandes</a></li>
+            <li><a href='/'>Adresses</a></li>
+            <li><a href='/'>Mot de passe</a></li>
+            <li><a href='/'>Détails du compte</a></li>
+            <li><a href='/'>Déconnexion</a></li>
+        </ul>
+    )
+}
+
+export default AccountMenu;
