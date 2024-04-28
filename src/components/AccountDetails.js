b89407abf024376951e291e0a7217b8c86da5e0b
SarahOtmane <sarahotmane02@gmail.com>
2024-04-28 15:55:38 +0200

    modif du lien envoye au composant fils Titre

--- src/components/AccountDetails.js
+++ src/components/AccountDetails.js
@@ -7,7 +7,7 @@ import Titre from "./Titre";
 const AccountDetails = () =>{
     return(
         <main>
-            <Titre titre="Mon compte" lien="/my-account/update-password" classe="backGris" />
+            <Titre titre="Mon compte" lien="/my-account/details" classe="backGris" />
             
             <div className='row account'>
                 <AccountMenu selected="details" />
