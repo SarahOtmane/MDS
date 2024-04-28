829dd17e3090a7c52277fc82573c9cb76bce7e4a
SarahOtmane <sarahotmane02@gmail.com>
2024-04-28 15:51:07 +0200

    premiere version accountDetails ok

--- src/components/AccountDetails.js
+++ src/components/AccountDetails.js
@@ -11,7 +11,22 @@ const AccountDetails = () =>{
             
             <div className='row account'>
                 <AccountMenu selected="details" />
-                
+                <section className='details'>
+                    <form className='details column'>
+                        <h2>Changer le mot de passe</h2>
+
+                        <lable className="text_bold">Nom</lable>
+                        <input type="text" placeholder="Cruz" />
+
+                        <lable className="text_bold">Prénom</lable>
+                        <input type="text" placeholder="Tom" />
+
+                        <lable className="text_bold">Email</lable>
+                        <input type="email" placeholder="test@gmail.com" />
+
+                        <button type='submit'>Sauvegarder les modifications</button>
+                    </form>
+                </section>
             </div>
         </main>
     )
