357f52e8704db2214bae422e6c7ce04f3bfd24de
SarahOtmane <sarahotmane02@gmail.com>
2024-05-13 22:12:15 +0200

    formulaire password forgot

---
+++ src/components/Sign/ForgetMdp.js
@@ -0,0 +1,21 @@
+import '../css/sign.css';
+import Titre from "../Titre.js";
+
+const ForgetMdp = () =>{
+    return(
+        <main>
+            <Titre titre="Mot de passe oublié" lien="/resetMdp" classe="backGris" />
+
+            <form className='formulaire column'>
+                <p>Veuillez saisir l'adresse e-mail associée à votre compte. Nous vous enverrons rapidement un lien pour réinitialiser votre mot de passe.</p>
+
+                <lable className="text_bold">Email</lable>
+                <input type="text" placeholder="email@gmail.com" />
+                
+                <button type='submit'>Envoyer le lien</button>
+            </form>
+        </main>
+    )
+}
+
+export default ForgetMdp;
