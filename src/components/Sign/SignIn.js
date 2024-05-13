1c433b8f506a44c27d6ffa093d56ef8830a65b37
SarahOtmane <sarahotmane02@gmail.com>
2024-05-13 22:12:35 +0200

    formulaire sign in

---
+++ src/components/Sign/SignIn.js
@@ -0,0 +1,26 @@
+import '../css/sign.css';
+import Titre from '../Titre';
+
+
+const SignIn = () =>{
+    return(
+        <main>
+            <Titre titre="Se connecter" lien="/login" classe="backGris" />
+
+            <form className='formulaire column'>
+                <lable className="text_bold">Email</lable>
+                <input type="text" placeholder="email@gmail.com" />
+
+                <lable className="text_bold">Mot de passe</lable>
+                <input type="password" placeholder="Mot de passe" />
+
+                <a href='/resetMdp'> Mot de passe oublié ?</a>
+
+                <button type='submit'>Se connecter</button>
+                <p>Vous n'avez pas de compte ? <a href='/register' className='text_bold'>S'inscrire</a></p>
+            </form>
+        </main>
+    )
+}
+
+export default SignIn;
