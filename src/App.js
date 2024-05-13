95391a9c2857d048dfd8564da8a60a52cded7d9c
SarahOtmane <sarahotmane02@gmail.com>
2024-05-13 22:26:32 +0200

    import homePage

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import './App.css';
 
 import Header from "./components/Header";
 import LandingPage from "./components/LandingPage";
+import Home from "./components/Home";
 import Footer from "./components/Footer";
+
 import SignUp from "./components/Sign/SignUp";
 import SignIn from "./components/Sign/SignIn";
 import ForgetMdp from "./components/Sign/ForgetMdp";
 import ResetMdp from "./components/Sign/ResetMdp";
+
 import CheckCart from "./components/Cart/CheckCart";
+
 import SuccefullOrder from "./components/Order/SuccefullOrder";
 import FailedOrder from "./components/Order/FailedOrder";
+
 import AccountOrder from "./components/Account/AccountOrder";
 import AccountPassword from "./components/Account/AccountPassword";
 import AccountDetails from "./components/Account/AccountDetails";
 import AccountAdress from "./components/Account/AccountAdress";
+
 
 
 
@@ -26,15 +32,21 @@ const App = () => {
                 <Header />
                 <Routes>
                     <Route path="/" element={<LandingPage />} />
+                    <Route path="/home" element={<Home />} />
 
                     <Route path="/user" element={<SignUp />}>
                         <Route path="register" element={<SignUp />} />
                         <Route path="login" element={<SignIn />} />
-                        <Route path="resetMdp" element={<ForgetMdp />} />
-                        <Route path="resetMdp/reset" element={<ResetMdp />} />
+
+                        <Route path="resetMdp" element={<ForgetMdp />}>
+                            <Route path="reset" element={<ResetMdp />} />
+                        </Route>
+
                         <Route path="cart/check" element={<CheckCart />} />
+
                         <Route path="success-order" element={<SuccefullOrder />} />
                         <Route path="failed-order" element={<FailedOrder />} />
+
                         <Route path="my-account" element={<AccountOrder />}>
                             <Route path="update-password" element={<AccountPassword />} />
                             <Route path="details" element={<AccountDetails />} />
