d7a474598730acacb92be3f853c4564c8c16a4e5
SarahOtmane <sarahotmane02@gmail.com>
2024-05-14 22:28:16 +0200

    ajout du titre

--- src/components/Legal/Conditions.js
+++ src/components/Legal/Conditions.js
@@ -1,9 +1,13 @@
-
+import Titre from '../Titre';
 
 
 
 const Conditions = () =>{
-
+    return(
+        <main>
+            <Titre titre="Conditions génrales" lien="/resetMdp" classe="backGris" />
+        </main>
+    )
 }
 
 export default Conditions;
