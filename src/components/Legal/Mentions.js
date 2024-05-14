5fed97e387145506f14034aafca285922f077ee0
SarahOtmane <sarahotmane02@gmail.com>
2024-05-14 22:27:35 +0200

    ajout du titre

--- src/components/Legal/Mentions.js
+++ src/components/Legal/Mentions.js
@@ -1,9 +1,13 @@
-
+import Titre from '../Titre';
 
 
 
 const Mentions = () =>{
-
+    return(
+        <main>
+            <Titre titre="Mentions légales" lien="/resetMdp" classe="backGris" />
+        </main>
+    )
 }
 
 export default Mentions;
