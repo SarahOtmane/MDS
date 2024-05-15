d96f14e0d623f8579668990c3cfa76abdca8e2bf
SarahOtmane <sarahotmane02@gmail.com>
2024-05-15 23:36:39 +0200

    repachoice

--- src/components/Reparation/RepaChoice.js
+++ src/components/Reparation/RepaChoice.js
@@ -1,9 +1,15 @@
-
+import Titre from '../Titre';
 
 const RepaChoice = () =>{
     return(
-        <main>
+        <main className="repaChoice">
+            <Titre titre="Je répare" lien="/user/reparation/choice" classe="backGris" />
+            <section className='section1'>
+                <h2 className='text_uppercase'>Pourquoi jeter <br></br> quand on peut réparé</h2>
+                <article>
 
+                </article>
+            </section>
         </main>
     )
 }
