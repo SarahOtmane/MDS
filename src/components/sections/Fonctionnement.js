5642ef9532dc8330eacdefbec279d7da0829fec5
SarahOtmane <sarahotmane02@gmail.com>
2024-05-22 21:46:30 +0200

    modif chemin import css

--- src/components/sections/Fonctionnement.js
+++ src/components/sections/Fonctionnement.js
@@ -1,15 +1,15 @@
-import image1 from '../assets/pictures/landingPage/image1.png';
-import image2 from '../assets/pictures/landingPage/image2.png';
-import image3 from '../assets/pictures/landingPage/image3.jpeg';
-import image4 from '../assets/pictures/landingPage/image4.png';
-import machine from '../assets/pictures/picto/machine.png';
-import ceintre from '../assets/pictures/picto/ceintre.png';
-import picto_Section4 from '../assets/pictures/picto/picto_section4.png';
-import loupe from '../assets/pictures/landingPage/loupe.svg';
-import check from '../assets/pictures/landingPage/check.svg';
-import send from '../assets/pictures/landingPage/send.svg';
-import ok from '../assets/pictures/landingPage/ok.svg';
-import arrow from '../assets/pictures/landingPage/Arrow.png';
+import image1 from '../../assets/pictures/landingPage/image1.png';
+import image2 from '../../assets/pictures/landingPage/image2.png';
+import image3 from '../../assets/pictures/landingPage/image3.jpeg';
+import image4 from '../../assets/pictures/landingPage/image4.png';
+import machine from '../../assets/pictures/picto/machine.png';
+import ceintre from '../../assets/pictures/picto/ceintre.png';
+import picto_Section4 from '../../assets/pictures/picto/picto_section4.png';
+import loupe from '../../assets/pictures/landingPage/loupe.svg';
+import check from '../../assets/pictures/landingPage/check.svg';
+import send from '../../assets/pictures/landingPage/send.svg';
+import ok from '../../assets/pictures/landingPage/ok.svg';
+import arrow from '../../assets/pictures/landingPage/Arrow.png';
 
 
 const Fonctionnement = () =>{
