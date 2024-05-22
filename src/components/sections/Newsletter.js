41d7da4f046c8127458d0176cdea46845e7ea298
SarahOtmane <sarahotmane02@gmail.com>
2024-05-22 22:10:45 +0200

    crea du composant newsletter

---
+++ src/components/sections/Newsletter.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+const Newsletter = () =>{
+    return(
+        <section className="newsletter column">
+            <h2>construisez une relation durables avec vos articles</h2>
+            <article className="row">
+                <div>
+                    <h3>abonnez vous à notre newsletter</h3>
+                    <span>Nous aimons vous surprendre avec des cadeaux occasionnels.</span>
+                </div>
+
+                <form className="row">
+                    <input placeholder="Votre e-mail" />
+                    <button>S'inscrire</button>
+                </form>
+            </article>
+        </section>
+    )
+}
+
+export default Newsletter;
