f23f706d3e46185f0d75422e9f8ac058e20f1d6e
SarahOtmane <sarahotmane02@gmail.com>
2024-05-22 21:57:40 +0200

    import des trois composant

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,16 @@
+import '../css/global.css';
+
+import Service from './sections/Service';
+import Fonctionnement from './sections/Fonctionnement';
+import Avis from './sections/Avis';
 
 const Home = () =>{
 
     return(
         <main className='home'>
-            
+            <Service />
+            <Fonctionnement />
+            <Avis />
         </main>
     )
 }
