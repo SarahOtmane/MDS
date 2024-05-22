650d814f9bc15a73767612713272d5fe225069a4
SarahOtmane <sarahotmane02@gmail.com>
2024-05-22 20:44:45 +0200

    element du composant UserSection de la section Pour qui

---
+++ src/components/sections/UserArtisan.js
@@ -0,0 +1,28 @@
+import particulier from '../assets/pictures/landingPage/particuliers.png';
+import artisan from '../assets/pictures/landingPage/artisans.png';
+import deco3 from '../assets/pictures/picto/deco3.png';
+import deco4 from '../assets/pictures/picto/deco4.png';
+
+
+const Fonctionnement = () =>{
+    return(
+        <section className='section4 backGris'>
+            <img className='picto' id="deco3" src={deco3} alt='' />
+            <img className='picto' id="deco4" src={deco4} alt='' />
+            <span className='text_uppercase text_bold'>RenoWear</span>
+            <h2 className='text_capitalize'>pour qui ?</h2>
+            <div className='row justifycontent_spbetween'>
+                <figure>
+                    <img src={particulier} alt='' />
+                    <p className='text_uppercase text_bold'>Les <br></br> particuliers</p>
+                </figure>
+                <figure>
+                    <img src={artisan} alt='' />
+                    <p className='text_uppercase text_bold'>Les <br></br> artisans</p>
+                </figure>
+            </div>
+        </section>
+    )
+}
+
+export default Fonctionnement;
