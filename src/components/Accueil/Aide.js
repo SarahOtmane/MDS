ed09e9c4488d5bd14a103d498a63364f84eac1e7
SarahOtmane <sarahotmane02@gmail.com>
2024-05-23 20:57:43 +0200

    l ajout des deux composant titre et newsletter

--- src/components/Accueil/Aide.js
+++ src/components/Accueil/Aide.js
@@ -1,10 +1,14 @@
 
 
+import Titre from "../Titre";
+import Newsletter from "../sections/Newsletter";
+
 
 const Aide = () =>{
     return(
         <main className="aide">
-
+            <Titre titre="Aide" lien="/aide" classe="backGris" />
+            <Newsletter />
         </main>
     )
 }
