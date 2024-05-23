04b67d47caeaa34054b4552a4faeb74b1f51b872
SarahOtmane <sarahotmane02@gmail.com>
2024-05-23 19:54:08 +0200

    add hr

--- src/components/Accueil/Concept.js
+++ src/components/Accueil/Concept.js
@@ -39,6 +39,7 @@ const Concept = () =>{
                         <h3>Le meilleur de la réparation</h3>
                         <p>On travaille main dans la main avec des artisans professionnels pour garantir que chaque articles a été réparé et remis en parfait état. </p>
                     </article>
+                    <hr></hr>
                     <article>
                         <h3>Meilleur pour la planète</h3>
                         <p>Comparé à un article neuf, un vêtement réparé permet d'éviter le gaspillage de 91,3% de matières premières et 86,4% d'eau. Mais il génère aussi 89% de déchets en moins et libère 91,6% d'émissions de carbone en moins dans l'atmosphère. </p>
