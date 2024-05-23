63ee26af8a6272a8e8989c264f85ba5fcb178255
SarahOtmane <sarahotmane02@gmail.com>
2024-05-23 11:53:38 +0200

    section1 du coomposant Concept

--- src/components/Concept.js
+++ src/components/Concept.js
@@ -1,11 +1,21 @@
-
+import Titre from './Titre';
+import Newsletter from './sections/Newsletter';
 
 
 
 const Concept = () =>{
     return(
-        <main className="concept">
-
+        <main className="concept backVert2">
+            <Titre titre="Le concept" lien="/concept" classe="backGris"  />
+            <section>
+                <h2>Hey, nous c'est Renowear</h2>
+                <p>
+                    Offrez une nouvelle vie à vos vêtements et accessoires. 
+                    Transformez vos pièces usées en créations uniques et personnalisées. 
+                    Plus abordable, meilleure pour la planète mais tout aussi performante.
+                </p>
+            </section>
+            <Newsletter />
         </main>
     )
 }
