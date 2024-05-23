23e5098d2101aa52ad08b6901fc3d69eef2386de
SarahOtmane <sarahotmane02@gmail.com>
2024-05-23 09:40:54 +0200

    ajout des classes

--- src/components/sections/Etape.js
+++ src/components/sections/Etape.js
@@ -13,22 +13,22 @@ const Etape = () =>{
         <section className="etape backRose2 column">
             <h3>Faites des affaires avec vos affaires</h3>
             <span>Réparez vos vêtements pour leurs offrir une seconce vie</span>
-            <div className="row container">
-                <article className="row">
+            <div className="row container justifycontent_spbetween">
+                <article className="row alignitem_center">
                     <img src={devis} alt='' />
                     <div>
                         <h4>1. Obtenez un devis</h4>
                         <p>Faites votre devis en ligne pour réparer vos articles en seulement 5 minutes </p>
                     </div>
                 </article>
-                <article className="row">
+                <article className="row alignitem_center">
                     <img src={depot} alt='' />
                     <div>
                         <h4>2. Déposez</h4>
                         <p>Vous avez 20 jours pour déposez vos vêtements/accessoires à réparer gratuitement</p>
                     </div>
                 </article>
-                <article className="row">
+                <article className="row alignitem_center">
                     <img src={recu} alt='' />
                     <div>
                         <h4>3. Recevez vos réparations</h4>
