b8bb03ab353c9a02288988a3d2a44bce92e4a754
SarahOtmane <sarahotmane02@gmail.com>
2024-05-23 20:33:24 +0200

    page concept ok !

--- src/components/Accueil/Concept.js
+++ src/components/Accueil/Concept.js
@@ -32,18 +32,14 @@ const Concept = () =>{
                 </article>
             </section>
             <section className='description'>
-                <img className='picto aiguilles' src={aiguilles} alt='' />
-                <img className='picto boutton' src={boutton} alt='' />
-                <img className='picto ceintre' src={ceintre} alt='' />
-                <img className='picto ciseaux' src={ciseaux} alt='' />
-                <img className='picto pelote' src={pelote} alt='' />
-                <img className='picto deco1' src={deco1} alt='' />
-                <img className='picto deco2' src={deco2} alt='' />
-                <img className='picto deco3' src={deco3} alt='' />
                 <div>
                     <article>
+                        <img className='picto boutton' src={boutton} alt='' />
+                        <img className='picto deco3' src={deco3} alt='' />
                         <h3>Une place de marché mondiale pour la réparation d’articles</h3>
+                        <img className='picto pelote' src={pelote} alt='' />
                         <p>RenoWear est une place de marché mondiale de réparation de vêtements. Grâce à notre processus de vérification rigoureux, notre charte qualité exigeante, seuls les meilleurs artisans peuvent nous rejoindre sur notre site et sublimé votre garde-robe.</p>
+                        <img className='picto ceintre' src={ceintre} alt='' />
                         <figure className='row justifycontent_spbetween alignitem_center'>
                             <img src={echos} alt="echos" />
                             <img src={sudOuest} alt="sudOuest" />
@@ -52,13 +48,17 @@ const Concept = () =>{
                     </article>
                     <hr></hr>
                     <article>
+                        <img className='picto ciseaux' src={ciseaux} alt='' />
                         <h3>Le meilleur de la réparation</h3>
                         <p>On travaille main dans la main avec des artisans professionnels pour garantir que chaque articles a été réparé et remis en parfait état. </p>
+                        <img className='picto aiguilles' src={aiguilles} alt='' />
                     </article>
                     <hr></hr>
                     <article>
                         <h3>Meilleur pour la planète</h3>
                         <p>Comparé à un article neuf, un vêtement réparé permet d'éviter le gaspillage de 91,3% de matières premières et 86,4% d'eau. Mais il génère aussi 89% de déchets en moins et libère 91,6% d'émissions de carbone en moins dans l'atmosphère. </p>
+                        <img className='picto deco1' src={deco1} alt='' />
+                        <img className='picto deco2' src={deco2} alt='' />
                     </article>
                 </div>
             </section>
