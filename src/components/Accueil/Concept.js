fc50a2e5627bad42b58cdc93df1878c0e995cdca
SarahOtmane <sarahotmane02@gmail.com>
2024-05-23 12:19:51 +0200

    texte de la section valeur du composant concept

--- src/components/Accueil/Concept.js
+++ src/components/Accueil/Concept.js
@@ -17,6 +17,23 @@ const Concept = () =>{
                     Plus abordable, meilleure pour la planète mais tout aussi performante.
                 </p>
             </section>
+            <section className='valeur'>
+                <div>
+                    <article>
+                        <h3>Une place de marché mondiale pour la réparation d’articles</h3>
+                        <p>RenoWear est une place de marché mondiale de réparation de vêtements. Grâce à notre processus de vérification rigoureux, notre charte qualité exigeante, seuls les meilleurs artisans peuvent nous rejoindre sur notre site et sublimé votre garde-robe.</p>
+                    </article>
+                    <hr></hr>
+                    <article>
+                        <h3>Le meilleur de la réparation</h3>
+                        <p>On travaille main dans la main avec des artisans professionnels pour garantir que chaque articles a été réparé et remis en parfait état. </p>
+                    </article>
+                    <article>
+                        <h3>Meilleur pour la planète</h3>
+                        <p>Comparé à un article neuf, un vêtement réparé permet d'éviter le gaspillage de 91,3% de matières premières et 86,4% d'eau. Mais il génère aussi 89% de déchets en moins et libère 91,6% d'émissions de carbone en moins dans l'atmosphère. </p>
+                    </article>
+                </div>
+            </section>
             <Newsletter />
         </main>
     )
