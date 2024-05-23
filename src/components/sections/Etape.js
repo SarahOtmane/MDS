ed383b46f4d12bd7d1d0a0f04160ff5a202ce5c1
SarahOtmane <sarahotmane02@gmail.com>
2024-05-23 09:44:49 +0200

    elements du composant etape okay

--- src/components/sections/Etape.js
+++ src/components/sections/Etape.js
@@ -14,21 +14,21 @@ const Etape = () =>{
             <h3>Faites des affaires avec vos affaires</h3>
             <span>Réparez vos vêtements pour leurs offrir une seconce vie</span>
             <div className="row container justifycontent_spbetween">
-                <article className="row alignitem_center">
+                <article className="row">
                     <img src={devis} alt='' />
                     <div>
                         <h4>1. Obtenez un devis</h4>
                         <p>Faites votre devis en ligne pour réparer vos articles en seulement 5 minutes </p>
                     </div>
                 </article>
-                <article className="row alignitem_center">
+                <article className="row">
                     <img src={depot} alt='' />
                     <div>
                         <h4>2. Déposez</h4>
                         <p>Vous avez 20 jours pour déposez vos vêtements/accessoires à réparer gratuitement</p>
                     </div>
                 </article>
-                <article className="row alignitem_center">
+                <article className="row">
                     <img src={recu} alt='' />
                     <div>
                         <h4>3. Recevez vos réparations</h4>
