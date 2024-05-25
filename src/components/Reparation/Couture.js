06d4a04e8bcd7319f5c087fc5d15d922d435e3e2
SarahOtmane <sarahotmane02@gmail.com>
2024-05-25 20:17:27 +0200

    import image

--- src/components/Reparation/Couture.js
+++ src/components/Reparation/Couture.js
@@ -1,11 +1,16 @@
+import '../../css/reparation.css';
 
 import Titre from "./Titre";
 
+import reparation from '../../assets/pictures/reparation.png';
 
 const Couture = () =>{
     return(
         <main className="couture">
             <Titre titre="Couture" lien="/reparation/couture" />
+            <section className="couture__contenu">
+                <img src={reparation} alt='' />
+            </section>
         </main>
     )
 }
