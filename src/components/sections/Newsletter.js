d94bbd2d7719442cd83cea2ca5386ee598d874ca
SarahOtmane <sarahotmane02@gmail.com>
2024-05-25 10:54:54 +0200

    delete h2

--- src/components/sections/Newsletter.js
+++ src/components/sections/Newsletter.js
@@ -6,7 +6,6 @@
 const Newsletter = () =>{
     return(
         <section className="newsletter column backGris">
-            <h2 className="text_uppercase">construisez une relation durables avec vos articles</h2>
             <article className="row">
                 <div>
                     <h3 className="text_capitalize">abonnez vous à notre newsletter</h3>
