bc8df8f50af18fab692229c4681cfab3c3c340ac
SarahOtmane <sarahotmane02@gmail.com>
2024-05-25 20:05:23 +0200

    import du composant titre dans le composant couture

--- src/components/Reparation/Couture.js
+++ src/components/Reparation/Couture.js
@@ -1,11 +1,11 @@
 
-
+import Titre from "./Titre";
 
 
 const Couture = () =>{
     return(
         <main className="couture">
-
+            <Titre titre="Couture" lien="/reparation/couture" />
         </main>
     )
 }
