42ccefe6620128f60962a3dce280bcd5c5f1fc3c
SarahOtmane <sarahotmane02@gmail.com>
2024-05-26 13:18:27 +0200

    add label

--- src/components/Reparation/Couture.js
+++ src/components/Reparation/Couture.js
@@ -9,14 +9,26 @@ const Couture = () =>{
     return(
         <main className="couture">
             <Titre titre="Couture" lien="/reparation/couture" />
-            <section className="couture__contenu row">
+            <section className="couture__contenu row justifycontent_spbetween">
                 <img src={reparation} alt='' />
                 <form>
+                    <label>Quelle est la catégorie ?</label>
                     <Input name="categorie" />
+
+                    <label>Quel est le type de vêtements ?</label>
                     <Input name="clotheType" />
+
+                    <label>Quelle est la matière du vêtement ?</label>
                     <Input name="clotheMatiere" />
+
+                    <label>Quelle est la réparation ?</label>
                     <Input name="reparationType" />
+
+                    <label>Quel est votre besoin ?</label>
                     <Input name="besoinType" />
+
+                    <label>Ajouter un commentaire</label>
+                    <textarea placeholder='Laissez un commentaire' rows={10} cols={40} />
                     
                 </form>
             </section>
