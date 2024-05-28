77766678938e227048df51824cd9d08c4538e39d
SarahOtmane <sarahotmane02@gmail.com>
2024-05-28 22:43:51 +0200

    element okay

--- src/components/Error/Error404.js
+++ src/components/Error/Error404.js
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 
 import Titre from "../Titre";
 import Newsletter from "../sections/Newsletter";
@@ -5,10 +6,20 @@ import Newsletter from "../sections/Newsletter";
 
 
 const Error404 = () =>{
+    const navigate = useNavigate();
+
     return(
         <main>  
             <Titre titre="Erreur" lien="/erreur404" classe="backGris" />
-
+            <section className="error">
+                <h1>OOPS ! Page introuvable</h1>
+                <p>
+                    Code erreur 404 <br></br>
+                    Ce lien n’a pas encore été réparé <br></br>
+                    Détendez-vous, tout va bien se passer ! <br></br>
+                </p>
+                <button onClick={() => navigate('/')}>Accéder à Renowear</button>
+            </section>
             <Newsletter />
         </main>
     )
