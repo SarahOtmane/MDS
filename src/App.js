c9a67e75e9e14213c452fa48063e2a010f5f58bb
SarahOtmane <sarahotmane02@gmail.com>
2024-05-28 19:55:32 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import './App.css';
 
-import LandingPage from "./components/LandingPage";
+import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 
@@ -77,9 +77,10 @@ const App = () => {
     return (
         <BrowserRouter>
             <>  
+                <Header />
                 <Routes>
-                    <Route path="/" element={<LandingPage />} />
-                    <Route path="/home" element={<Home />} />
+                    {/* <Route path="/" element={<LandingPage />} /> */}
+                    <Route path="/" element={<Home />} />
                     <Route path='/concept' element={<Concept />} />
                     <Route path="/aide" element={<Aide />} />
                     <Route path="/repare" element={<Repare />} />
