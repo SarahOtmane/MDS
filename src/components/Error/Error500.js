620550fc272649244fa71cc85510a2b351f1bc3a
SarahOtmane <sarahotmane02@gmail.com>
2024-05-28 23:22:09 +0200

    error 500

--- src/components/Error/Error500.js
+++ src/components/Error/Error500.js
@@ -1,3 +1,6 @@
+import { useNavigate } from "react-router-dom";
+
+import '../../css/error.css';
 
 import Titre from "../Titre";
 import Newsletter from "../sections/Newsletter";
@@ -5,10 +8,21 @@ import Newsletter from "../sections/Newsletter";
 
 
 const Error500 = () =>{
+    const navigate = useNavigate();
+
     return(
         <main>  
             <Titre titre="Erreur" lien="/erreur404" classe="backGris" />
-
+            <section className="error">
+                <div>
+                <h1>SORRY ! Erreur du serveur</h1>
+                <p>
+                    Code erreur 500 <br></br>
+                    Détendez-vous, tout va bien se passer ! <br></br>
+                </p>
+                <button onClick={() => navigate('/')}>Accéder à Renowear</button>
+                </div>
+            </section>
             <Newsletter />
         </main>
     )
