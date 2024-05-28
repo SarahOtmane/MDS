536b93bdeda38b7e56d0e4deb27fc466f3981442
SarahOtmane <sarahotmane02@gmail.com>
2024-05-28 23:14:16 +0200

    error 403 ok

--- src/components/Error/Error403.js
+++ src/components/Error/Error403.js
@@ -1,3 +1,6 @@
+import { useNavigate } from "react-router-dom";
+
+import '../../css/error.css';
 
 import Titre from "../Titre";
 import Newsletter from "../sections/Newsletter";
@@ -5,10 +8,21 @@ import Newsletter from "../sections/Newsletter";
 
 
 const Error403 = () =>{
+    const navigate = useNavigate();
+
     return(
         <main>  
-            <Titre titre="Erreur" lien="/erreur404" classe="backGris" />
-
+            <Titre titre="Erreur" lien="/erreur403" classe="backGris" />
+            <section className="error">
+                <div>
+                <h1>OOPS ! Accès non autorisée</h1>
+                <p>
+                    Code erreur 403 <br></br>
+                    Détendez-vous, tout va bien se passer ! <br></br>
+                </p>
+                <button onClick={() => navigate('/')}>Accéder à Renowear</button>
+                </div>
+            </section>
             <Newsletter />
         </main>
     )
