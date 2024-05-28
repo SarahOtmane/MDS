1e3d6018190eaa10239229af12dd2537483cc2aa
SarahOtmane <sarahotmane02@gmail.com>
2024-05-28 19:34:01 +0200

    delete img picto and add it in background iumage

--- src/components/sections/UserArtisan.js
+++ src/components/sections/UserArtisan.js
@@ -1,14 +1,11 @@
 import particulier from '../../assets/pictures/landingPage/particuliers.png';
 import artisan from '../../assets/pictures/landingPage/artisans.png';
-import deco3 from '../../assets/pictures/picto/deco3.png';
-import deco4 from '../../assets/pictures/picto/deco4.png';
 
 
 const UserArtisan = () =>{
     return(
         <section className='userArtisan backGris'>
-            <img className='picto' id="deco3" src={deco3} alt='' />
-            <img className='picto' id="deco4" src={deco4} alt='' />
+            <section>
             <span className='text_uppercase text_bold'>RenoWear</span>
             <h2 className='text_capitalize'>pour qui ?</h2>
             <div className='row justifycontent_spbetween'>
@@ -21,6 +18,7 @@ const UserArtisan = () =>{
                     <p className='text_uppercase text_bold'>Les <br></br> artisans</p>
                 </figure>
             </div>
+            </section>
         </section>
     )
 }
