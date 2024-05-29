aaf975b36ef087e32973eec0974b28f70942befb
SarahOtmane <sarahotmane02@gmail.com>
2024-05-29 23:18:39 +0200

    composant signup artisan

---
+++ src/components/Artisan/Sign/SignUp.js
@@ -0,0 +1,13 @@
+
+import Titre from '../../Titre';
+
+
+const ArtisanSignUp = () =>{
+    return(
+        <main className='artisan_signup'>
+            <Titre titre="S'inscrire" lien="/artisan/register" classe="backGris" />
+        </main>
+    )
+}
+
+export default ArtisanSignUp;
