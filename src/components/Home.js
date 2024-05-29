249c44f036a04a66a7650b90d0e563eeba377111
SarahOtmane <sarahotmane02@gmail.com>
2024-05-29 21:34:30 +0200

    composant okay

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,11 +6,39 @@ import Avis from './sections/Avis';
 import Newsletter from './sections/Newsletter';
 import Slider from './sections/Slider';
 
+import satisfaction from '../assets/pictures/home/satisfaction.svg';
+import paiement from '../assets/pictures/home/paiement.svg';
+import serviceClient from '../assets/pictures/home/serviceClient.svg'
+
 const Home = () =>{
+    const elements=[{
+        img: serviceClient,
+        titre: 'Service client',
+        text: 'Déposez votre vêtement chez l\'artisan et redonnez-lui une seconde vie.'
+    },{
+        img: satisfaction,
+        titre: 'Satisfaction garentie',
+        text: 'Achetez en toute confiance grâce à notre garantie de satisfaction : aimez-le ou obtenez un remboursement.'
+    },{
+        img: paiement,
+        titre: 'Paiement sécurisé',
+        text: 'Votre sécurité est notre priorité. Vos paiements sont sécurisés chez nous.'
+    }]
 
     return(
         <main className='home'>
             <Slider />
+            <section className='elements row justifycontent_spbetween'>
+                {elements.map((element, index) => (
+                    <article
+                        key={index}
+                    >
+                        <img src={element.img} alt='' />
+                        <h2>{element.titre}</h2>
+                        <p>{element.text}</p>
+                    </article>
+                ))}
+            </section>
             <Service />
             <Fonctionnement />
             <Avis />
