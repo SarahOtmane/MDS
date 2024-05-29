cfb3c0a90e6dc3018b14b01faa714a5ba5e2077c
SarahOtmane <sarahotmane02@gmail.com>
2024-05-29 21:14:53 +0200

    supprimer le composant etape

--- src/components/Accueil/Repare.js
+++ src/components/Accueil/Repare.js
@@ -1,7 +1,6 @@
 import '../../css/accueil.css';
 
 import Titre from '../Titre';
-import Etape from '../sections/Etape';
 import Newsletter from '../sections/Newsletter';
 
 
@@ -10,7 +9,6 @@ const Repare = () =>{
         <main className="repare">
             <Titre titre="Je répare" lien="/repare" classe="backGris"  />
             <h2 className='etape text_uppercase'>pourquoi jeter quand on peut réparer</h2>
-            <Etape />
             <Newsletter />
         </main>
     )
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,6 @@ import Service from './sections/Service';
 import Fonctionnement from './sections/Fonctionnement';
 import Avis from './sections/Avis';
 import Newsletter from './sections/Newsletter';
-import Etape from './sections/Etape';
 import Slider from './sections/Slider';
 
 const Home = () =>{
@@ -14,7 +13,6 @@ const Home = () =>{
             <Slider />
             <Service />
             <Fonctionnement />
-            <Etape />
             <Avis />
             <h2 className="text_uppercase newsletter backGris">construisez une relation durables avec vos articles</h2>
             <Newsletter />
