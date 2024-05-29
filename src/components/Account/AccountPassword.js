bce7e15409ce8a61bff1d901e123407f7a5ccd74
SarahOtmane <sarahotmane02@gmail.com>
2024-05-29 14:53:37 +0200

    form update password

--- src/components/Account/AccountPassword.js
+++ src/components/Account/AccountPassword.js
@@ -1,3 +1,7 @@
+import React, { useEffect, useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import axiosInstance, { removeToken }  from '../../axiosConfig';
+
 import '../../css/account.css';
 
 import AccountMenu from './AccountMenu';
@@ -5,6 +9,85 @@ import Titre from "../Titre";
 
 
 const AccountPassword = () =>{
+    const navigate = useNavigate();
+
+    const [user, setUser] = useState({});
+
+    const [formData, setFormData]=useState({
+        oldPassword:'',
+        confirmPassword:'',
+        newPassword:'',
+    });
+
+    const [newErrors, setNewErrors] = useState({});
+
+    useEffect(() => {
+        const getUser = async () => {
+            try {
+                const response = await axiosInstance.get('/users');
+                setUser(response.data);
+            } catch (error) {
+                console.error('Erreur lors de la récupération des informations de l utilisateur:', error);
+            }
+        };
+
+        getUser();
+    }, []);
+
+    const updateChamps = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value.trim()
+        });
+    };
+
+    const validate = () => {
+        const newErrors = {};
+    
+        // Password validation
+        const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{8,}$/;
+        if (!passwordRegex.test(formData.password)) {
+          newErrors.newPassword = 'Le mot de passe doit contenir au moins 8 caractères, dont une majuscule, une minuscule, un chiffre et un caractère spécial';
+        }
+
+        //Conformité des deux password
+        if(formData.oldPassword !== formData.newPassword){
+            newErrors.oldPassword = 'Les deux mot de passes ne sont pas conformes '
+        }
+
+        if(formData.oldPassword === formData.newPassword){
+            newErrors.samePassword = 'Le nouveau mot de passe est le même que l\'ancien '
+        }
+
+        setNewErrors(newErrors);
+
+        //retourne true ou false en fonction de si ya des erreurs ou pas
+        return Object.keys(newErrors).length === 0;
+    };
+
+    const submitForm = async (e) => {
+        e.preventDefault();
+    
+        if (!validate()) {
+          return;
+        }
+
+        setUser({
+            ...user,
+            password: formData.newPassword,
+        })
+    
+        try {
+            await axiosInstance.put('users', user);
+            removeToken();
+            navigate('/use/login');
+        } catch (error) {
+            console.error('Erreur lors de l\'enregistrement de l\'utilisateur:', error)      
+        }
+    };
+
+
     return(
         <main>
             <Titre titre="Mon compte" lien="/user/my-account/update-password" classe="backGris" />
@@ -12,17 +95,42 @@ const AccountPassword = () =>{
             <div className='row account'>
                 <AccountMenu selected="motdepasse" />
                 <section className='updateMdp'>
-                    <form className='updateMdp column'>
+                    <form className='updateMdp column' onSubmit={submitForm}>
                         <h2>Changer le mot de passe</h2>
 
-                        <lable className="text_bold">Ancien mot de passe</lable>
-                        <input type="password" placeholder="Mot de passe" />
+                        <label className="text_bold">Ancien mot de passe</label>
+                        <input 
+                            type="password" 
+                            name='oldPassword'
+                            placeholder="Mot de passe" 
+                            defaultValue={formData.oldPassword}
+                            onChange={updateChamps}
+                            required
+                        />
+                        {newErrors.passwordIncorrect && <p style={{ color: 'red' }}>{newErrors.passwordIncorrect}</p>}
 
-                        <lable className="text_bold">Nouveau mot de passe</lable>
-                        <input type="password" placeholder="Mot de passe" />
+                        <label className="text_bold">Nouveau mot de passe</label>
+                        <input 
+                            type="password" 
+                            name='confirmPassword'
+                            placeholder="Mot de passe" 
+                            defaultValue={formData.confirmPassword}
+                            onChange={updateChamps}
+                            required
+                        />
+                        {newErrors.oldPassword && <p style={{ color: 'red' }}>{newErrors.oldPassword}</p>}
 
-                        <lable className="text_bold">Confirmer le mot de passe</lable>
-                        <input type="password" placeholder="Mot de passe" />
+                        <label className="text_bold">Confirmer le mot de passe</label>
+                        <input 
+                            type="password" 
+                            name='newPassword'
+                            placeholder="Mot de passe" 
+                            defaultValue={formData.newPassword}
+                            onChange={updateChamps}
+                            required
+                        />
+                        {newErrors.newPassword && <p style={{ color: 'red' }}>{newErrors.newPassword}</p>}
+                        {newErrors.samePassword && <p style={{ color: 'red' }}>{newErrors.samePassword}</p>}
 
                         <button type='submit'>Réinitialiser le mot de passe</button>
                     </form>
