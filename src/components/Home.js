830dc3a03bc8650c6622cd4e4bcc556ad02a3af2
SarahOtmane <sarahotmane02@gmail.com>
2024-05-29 20:08:53 +0200

    import du composat slider

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,13 @@ import Fonctionnement from './sections/Fonctionnement';
 import Avis from './sections/Avis';
 import Newsletter from './sections/Newsletter';
 import Etape from './sections/Etape';
+import Slider from './sections/Slider';
 
 const Home = () =>{
 
     return(
         <main className='home'>
+            <Slider />
             <Service />
             <Fonctionnement />
             <Etape />
