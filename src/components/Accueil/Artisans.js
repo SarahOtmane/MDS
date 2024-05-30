a704d9707858ea07f2aa34eb2b1ea40d21228070
SarahOtmane <sarahotmane02@gmail.com>
2024-05-30 17:11:18 +0200

    crea composant pour la page nos composants

---
+++ src/components/Accueil/Artisans.js
@@ -0,0 +1,13 @@
+
+import Titre from "../Titre";
+
+
+const Artisans = () =>{
+    return(
+        <main className="artisans">
+            <Titre titre="Nos artisans" lien="/artisans" classe="backGris" />
+        </main>
+    )
+}
+
+export default Artisans;
