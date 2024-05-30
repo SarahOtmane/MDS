9f647c6f42e10a43792ee326be343a5113e28b77
SarahOtmane <sarahotmane02@gmail.com>
2024-05-30 17:18:18 +0200

    import composant enfant

--- src/components/Accueil/Artisans.js
+++ src/components/Accueil/Artisans.js
@@ -1,11 +1,14 @@
 
 import Titre from "../Titre";
+import Section1NosArtisans from "../sections/Section1NosArtisans";
+
 
 
 const Artisans = () =>{
     return(
         <main className="artisans">
             <Titre titre="Nos artisans" lien="/artisans" classe="backGris" />
+            <Section1NosArtisans />
         </main>
     )
 }
