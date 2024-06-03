6bbd723b0c0bcca9274bf7b3258f6ce381dd3c38
SarahOtmane <sarahotmane02@gmail.com>
2024-06-03 11:53:53 +0200

    section repareService

--- src/components/Accueil/Repare.js
+++ src/components/Accueil/Repare.js
@@ -11,6 +11,24 @@ const Repare = ({command, setCommand}) =>{
             <Titre titre="Je répare" lien="/repare" classe="backGris"  />
             <h2 className='etape text_uppercase'>pourquoi jeter <br></br> quand on peut réparer</h2>
             <Etape />
+            <section className='repareService'>
+                <h2>Quel service souhaitez-vous choisir?</h2>
+                <p>Nos ateliers-boutiques répartis dans toute la France, vous ouvrent leurs portes pour vous rendre service.</p>
+                <form className='row justifycontent_spbetween'>
+                    <article>
+                        <div></div>
+                        <p className='text_uppercase backVert2'>Services couture</p>
+                    </article>
+                    <article>
+                        <div></div>
+                        <p className='text_uppercase backVert2'>Services couture</p>
+                    </article>
+                    <article>
+                        <div></div>
+                        <p className='text_uppercase backVert2'>Services couture</p>
+                    </article>
+                </form>
+            </section>
             <Newsletter />
         </main>
     )
