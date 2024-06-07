c7f9fcb584357151a9d2d1e89788d02e13de63f4
SarahOtmane <sarahotmane02@gmail.com>
2024-06-08 00:09:47 +0200

    update des paragraphes

--- src/components/Accueil/Concept.js
+++ src/components/Accueil/Concept.js
@@ -27,7 +27,7 @@ const Concept = () =>{
                 <div>
                     <article>
                         <h3>Une place de marché mondiale pour la réparation d’articles</h3>
-                        <p>RenoWear est une place de marché mondiale de réparation de vêtements. Grâce à notre processus de vérification rigoureux, notre charte qualité exigeante, seuls les meilleurs artisans peuvent nous rejoindre sur notre site et sublimé votre garde-robe.</p>
+                        <p>RenoWear est une place de marché mondiale de réparation de vêtements. Grâce à notre processus de vérification rigoureux, notre charte qualité exigeante, seuls les meilleurs artisans peuvent nous rejoindre sur notre site et sublimé votre garde-robe, pour une mode durable.</p>
                         <figure className='row justifycontent_spbetween alignitem_center'>
                             <img src={echos} alt="echos" />
                             <img src={sudOuest} alt="sudOuest" />
@@ -37,12 +37,12 @@ const Concept = () =>{
                     <hr></hr>
                     <article>
                         <h3>Le meilleur de la réparation</h3>
-                        <p>On travaille main dans la main avec des artisans professionnels pour garantir que chaque articles a été réparé et remis en parfait état. </p>
+                        <p>On travaille main dans la main avec des artisans professionnels, français pour garantir que chaque article a été réparé et remis en parfait état.  </p>
                     </article>
                     <hr></hr>
                     <article>
                         <h3>Meilleur pour la planète</h3>
-                        <p>Comparé à un article neuf, un vêtement réparé permet d'éviter le gaspillage de 91,3% de matières premières et 86,4% d'eau. Mais il génère aussi 89% de déchets en moins et libère 91,6% d'émissions de carbone en moins dans l'atmosphère. </p>
+                        <p>Comparé à un article neuf, un vêtement réparé permet d'éviter le gaspillage de 2 700 litres d'eau. Mais il génère aussi 79% de déchets en moins et libère 50% d'émissions de carbone en moins dans l'atmosphère.  </p>
                     </article>
                 </div>
             </section>
