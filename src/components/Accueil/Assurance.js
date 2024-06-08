edff5df9153b381af5ed64e56b58261cb0370338
SarahOtmane <sarahotmane02@gmail.com>
2024-06-08 10:56:01 +0200

    section 1

--- src/components/Accueil/Assurance.js
+++ src/components/Accueil/Assurance.js
@@ -8,8 +8,17 @@ import Newsletter from '../sections/Newsletter';
 
 const Assurance = () =>{
     return(
-        <main className="assurance">
+        <main className="concept rejoindre assurance">
             <Titre titre="Assurance" lien="/rejoindre" classe="backGris"  />
+            <section className='presentation'>
+                <article>
+                    <h2>Un partenariat avec <br></br> AXA</h2>
+                    <p>
+                        La plateforme idéale pour connecter les particuliers avec des artisans experts en couture, maroquinerie, et bien plus encore. Notre mission est de donner une nouvelle vie à vos articles préférés, en garantissant une réparation de qualité.
+                    </p>
+                </article>
+                
+            </section>
             <Newsletter />
         </main>
     )
