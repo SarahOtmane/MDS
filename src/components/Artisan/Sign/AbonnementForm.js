3fb36dcdaafa28c2720fb21f5d688685fc5ced36
SarahOtmane <sarahotmane02@gmail.com>
2024-06-10 12:23:58 +0200

    update content

--- src/components/Artisan/Sign/AbonnementForm.js
+++ src/components/Artisan/Sign/AbonnementForm.js
@@ -6,6 +6,12 @@ const AbonnementForm = () =>{
     return(
         <section className="abonnement">
             <div></div>
+            <article className='adhesion'>
+                <h4>Frais d’adhésion</h4>
+                <span>24,99 € *</span>
+                <p>* Montant unique pour l'inscription initiale qui peut couvrir les coûts administratifs, la création de profil, et l'accès aux outils de base du site.</p>
+                <button className='black'>Rejoindre RenoWear</button>
+            </article>
         </section>
     )
 }
