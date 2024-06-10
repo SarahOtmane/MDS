ad528efef38882eeb649112384021f2751fe36c7
SarahOtmane <sarahotmane02@gmail.com>
2024-06-10 13:18:21 +0200

    update css et element du composant abonnement

--- src/components/Artisan/Sign/AbonnementForm.js
+++ src/components/Artisan/Sign/AbonnementForm.js
@@ -10,7 +10,7 @@ const AbonnementForm = () =>{
                 <h4>Frais d’adhésion</h4>
                 <span>24,99 € *</span>
                 <p>* Montant unique pour l'inscription initiale qui peut couvrir les coûts administratifs, la création de profil, et l'accès aux outils de base du site.</p>
-                <button className='black'>Rejoindre RenoWear</button>
+                <button className='black'>Je m'inscris</button>
             </article>
         </section>
     )
