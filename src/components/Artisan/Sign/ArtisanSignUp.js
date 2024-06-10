9a156e009ef35c2756f7e04c9d5c063f8324d050
SarahOtmane <sarahotmane02@gmail.com>
2024-06-10 11:47:23 +0200

    remove le fomulaire poour les details et import du composant DetailForm

--- src/components/Artisan/Sign/ArtisanSignUp.js
+++ src/components/Artisan/Sign/ArtisanSignUp.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 import '../../../css/artisanSign.css';
 
 import Titre from '../../Titre';
+import DetailForm from './DetailForm';
 
 
 const ArtisanSignUp = () =>{
@@ -19,6 +20,7 @@ const ArtisanSignUp = () =>{
         //4. suite : on recup l etape du formulaire
         //5. errors, emailExist: gestion des erreurs
         //6. buttonAble : able and desable le bouton suivant
+        //7. showPassword / showConfirmPassword : voir/cacher mot de passe
     //les fonction :
         //1. suiteForm : passer d'une etape du formulaire a la suivante
         //2. submitForm : envoyer les données de l artisan a la bdd
@@ -32,6 +34,8 @@ const ArtisanSignUp = () =>{
     const [jobs, setJobs] = useState([]);
     const [prestations, setPrestations] = useState([]);
     const [buttonAble, setButtonAble] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
     useEffect(() => {
         const getJob = async () => {
@@ -48,7 +52,8 @@ const ArtisanSignUp = () =>{
 
     const [suite, setSuite] = useState({
         metier: false,
-        reparation: false
+        reparation: false,
+        abonnement: false
     });
     
     const [formData, setFormData] = useState({
@@ -56,6 +61,7 @@ const ArtisanSignUp = () =>{
         firstname: '',
         email: '',
         password: '',
+        confirmPassword: '',
         mobile: '',
         streetAdress: '',
         city: '',
@@ -69,6 +75,9 @@ const ArtisanSignUp = () =>{
 
     const [errors, setErrors] = useState({});
     const [emailExist, setEmailExist] = useState(false);
+
+    const passwordVisible = () =>{setShowPassword(!showPassword)};
+    const confirmPasswordVisible = () =>{setShowConfirmPassword(!showConfirmPassword)};
 
     const updateChamps = (e) => {
         const { name, value } = e.target;
@@ -157,147 +166,15 @@ const ArtisanSignUp = () =>{
         <main className='artisan_signup'>
             <Titre titre="S'inscrire" lien="/artisan/register" classe="backGris" />
 
-            {(!suite.metier && !suite.reparation) && (
-                <form className='detailsArtisan' onSubmit={(e) => suiteForm(e, 'metier')}>
-                    <section className='row justifycontent_spbetween'>
-                    <div className='column'>
-                        <label className="text_bold">Nom</label>
-                        <input 
-                            type="text" 
-                            name='lastname'
-                            placeholder="Cruz" 
-                            defaultValue={formData.lastname}
-                            onChange={updateChamps}
-                            required
-                        />
-
-                        <label className="text_bold">Prénom</label>
-                        <input 
-                            type="text" 
-                            name='firstname'
-                            placeholder="Tom" 
-                            defaultValue={formData.firstname}
-                            onChange={updateChamps}
-                            required
-                        />
-
-                        <label className="text_bold">Email</label>
-                        <input 
-                            type="email" 
-                            name="email"
-                            placeholder="email@gmail.com" 
-                            defaultValue={formData.email}
-                            onChange={updateChamps}
-                            required
-                        />
-                        {errors.email && <p style={{ color: 'red' }}>{errors.email}</p>}
-                        {emailExist && <p style={{ color: 'red' }}>Cet email est associé à un compte existant.</p>}
-
-                        <label className="text_bold">Téléphone</label>
-                        <input 
-                            type="tel" 
-                            pattern="[0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2}" 
-                            name='mobile'
-                            placeholder="06 36 46 79 12" 
-                            defaultValue={formData.mobile}
-                            onChange={updateChamps}
-                            required
-                        />
-                        {errors.mobile && <p style={{ color: 'red' }}>{errors.mobile}</p>}
-
-                        <label className="text_bold">Mot de passe</label>
-                        <input 
-                            type="password" 
-                            name='password'
-                            placeholder="Mot de passe" 
-                            defaultValue={formData.password}
-                            onChange={updateChamps}
-                            required
-                        />
-                        {errors.password && <p style={{ color: 'red' }}>{errors.password}</p>}
-
-                        <label className="text_bold">Confirmer votre mot de passe</label>
-                        <input 
-                            type="password" 
-                            name='password'
-                            placeholder="Mot de passe" 
-                            defaultValue={formData.password}
-                            onChange={updateChamps}
-                            required
-                        />
-
-                        
-                    </div>
-                    <div className='column'>
-                        <label className="text_bold">Adresse</label>
-                        <input 
-                            type="text" 
-                            name='streetAdress'
-                            placeholder="Tom" 
-                            defaultValue={formData.streetAdress}
-                            onChange={updateChamps}
-                            required
-                        />
-
-                        <label className="text_bold">Ville</label>
-                        <input 
-                            type="text" 
-                            name='city'
-                            placeholder="Tom" 
-                            defaultValue={formData.city}
-                            onChange={updateChamps}
-                            required
-                        />
-
-                        <label className="text_bold">Code Postal</label>
-                        <input 
-                            type="text" 
-                            name='postalCode'
-                            placeholder="Tom" 
-                            defaultValue={formData.postalCode}
-                            onChange={updateChamps}
-                            required
-                        />
-
-                        <label className="text_bold">Pays</label>
-                        <input 
-                            type="text" 
-                            name='country'
-                            placeholder="Tom" 
-                            defaultValue={formData.country}
-                            onChange={updateChamps}
-                            required
-                        />
-                        <label className="text_bold">Numéro TVA</label>
-                        <input 
-                            type="text" 
-                            name='numeroTVA'
-                            placeholder="FR 93 602 036 444" 
-                            defaultValue={formData.numeroTVA}
-                            onChange={updateChamps}
-                            required
-                        />
-
-                        <label className="text_bold">SIRET</label>
-                        <input 
-                            type="text" 
-                            name='siret'
-                            placeholder="602 036 444 04227" 
-                            defaultValue={formData.siret}
-                            onChange={updateChamps}
-                            required
-                        />
-                    </div>
-                    </section>
-
-                    <p>En créant un compte, vous acceptez nos conditions d'utilisations et notre politique de confidentialité</p>
-
-                    <button type='submit' className='black'>Créer un compte</button>
-                    <p>Vous avez déja un compte ? <a href='/artisan/login' className='text_bold'>Se connecter</a></p>
-                </form>
+            {(!suite.metier && !suite.reparation && !suite.abonnement) && (
+                <DetailForm 
+                    suiteForm = {suiteForm}  updateChamps={updateChamps}  formData={formData}
+                    errors={errors}  showPassword={showPassword} showConfirmPassword={showConfirmPassword}
+                    passwordVisible={passwordVisible}  confirmPasswordVisible={confirmPasswordVisible} emailExist={emailExist}
+                />
             )}
 
-            {(suite.metier && !suite.reparation) && (
+            {(suite.metier && !suite.reparation && !suite.abonnement) && (
                 <form onSubmit={(e) => suiteForm(e, 'reparation')} className='jobArtisan'>
                     <h2>Métier</h2>
                     <p>Choisissez votre domaine d’expertise et rejoignez une communauté</p>
@@ -322,8 +199,8 @@ const ArtisanSignUp = () =>{
                 </form>
             )}
 
-            {(suite.metier && suite.reparation) && (
-                <form className='jobArtisan reparationArtisan' onSubmit={submitForm}>
+            {(suite.metier && suite.reparation && !suite.abonnement) && (
+                <form className='jobArtisan reparationArtisan' onSubmit={(e) => suiteForm(e, 'abonnement')}>
                     <h2>Pour quelles expertises souhaitez-vous proposez vos services ?</h2>
                     <p>Choisissez-en autant que vous voulez !</p>
                     <article className='job_selection column'>
@@ -352,6 +229,10 @@ const ArtisanSignUp = () =>{
                     </button>
                 </form>
             )}
+
+            {/* {(suite.metier && suite.reparation && suite.abonnement) && (
+
+            )} */}
 
         </main>
     )
