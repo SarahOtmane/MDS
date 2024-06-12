58e6a355f250478969cccdf08e79bb7e27d8c166
SarahOtmane <sarahotmane02@gmail.com>
2024-06-12 22:26:12 +0200

    update svg

--- src/components/Reparation/Titre.js
+++ src/components/Reparation/Titre.js
@@ -7,11 +7,11 @@ const Titre = ({titre, lien}) =>{
             <article className="row alignitem_center">
                 <a href="/">RenoWear</a>
                 <svg width="12" height="26" viewBox="0 0 6 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M5.81168 5.96731C6.06095 6.24435 6.06303 6.69578 5.81633 6.97571L0.948585 12.5L0.0460425 11.4967L4.46723 6.47935L0 1.51366L0.89321 0.5L5.81168 5.96731Z" fill="#71747E"/>
+                    <path fillRule="evenodd" clipRule="evenodd" d="M5.81168 5.96731C6.06095 6.24435 6.06303 6.69578 5.81633 6.97571L0.948585 12.5L0.0460425 11.4967L4.46723 6.47935L0 1.51366L0.89321 0.5L5.81168 5.96731Z" fill="#71747E"/>
                 </svg>
                 <a href="/repare">Je répare</a>
                 <svg width="12" height="26" viewBox="0 0 6 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M5.81168 5.96731C6.06095 6.24435 6.06303 6.69578 5.81633 6.97571L0.948585 12.5L0.0460425 11.4967L4.46723 6.47935L0 1.51366L0.89321 0.5L5.81168 5.96731Z" fill="#71747E"/>
+                    <path fillRule="evenodd" clipRule="evenodd" d="M5.81168 5.96731C6.06095 6.24435 6.06303 6.69578 5.81633 6.97571L0.948585 12.5L0.0460425 11.4967L4.46723 6.47935L0 1.51366L0.89321 0.5L5.81168 5.96731Z" fill="#71747E"/>
                 </svg>
                 <a href={lien} className='text_bold'>{titre}</a>
             </article>
