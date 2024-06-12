a38192399388c32e06f78fca368a27385b2dc02e
SarahOtmane <sarahotmane02@gmail.com>
2024-06-12 08:54:26 +0200

    update composnt arr artisans

--- src/components/sections/AllArtisan.js
+++ src/components/sections/AllArtisan.js
@@ -1,27 +1,24 @@
 
 
 
-const AllArtisan = ({artisans}) =>{
-    let noArtisan = artisans.length === 0;
-
-    return(
+const AllArtisan = ({ artisans, notFound }) => {
+    return (
         <>
-            {noArtisan && 
+            {notFound ? (
                 <section className="noArtisan">
                     <h2>Désolé, nous ne réparons pas encore ici !</h2>
                     <p>On souhaite rendre la réparation accessible à tous alors on y travaille !</p>
                 </section>
-            }
-            {!noArtisan && 
+            ) : (
                 <section className="allArtisan">
                     {artisans.map(artisan => (
                         <article key={artisan.id}>
                         </article>
                     ))}  
                 </section>
-            }
+            )}
         </>
-    )
-}
+    );
+};
 
 export default AllArtisan;
