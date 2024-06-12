b3b2afd8026679d57b4a6250256586965569fb04
SarahOtmane <sarahotmane02@gmail.com>
2024-06-12 22:21:52 +0200

    update svg propreties

--- src/components/Reparation/Reparation.js
+++ src/components/Reparation/Reparation.js
@@ -12,9 +12,9 @@ const Reparation = () =>{
             <Titre titre="Couture" lien="/user/reparation/details" />
             <section className='loading column'>
                 <p>1/3 Votre réparation</p>
-                <div class="progress-bar">
-                    <div class="green"></div>
-                    <div class="black"></div>
+                <div className="progress-bar">
+                    <div className="green"></div>
+                    <div className="black"></div>
                 </div>
             </section>
             <section className="couture__contenu row justifycontent_spbetween">
@@ -38,8 +38,8 @@ const Reparation = () =>{
                     <button className='row'>
                         Continuer 
                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M18.2896 11.4958C18.5701 11.7743 18.5701 12.2257 18.2896 12.5042L12.7541 18L11.7384 16.9916L16.766 12L11.7384 7.00845L12.7541 6L18.2896 11.4958Z" fill="white"/>
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M17.9972 12.7131H5.5V11.2869H17.9972V12.7131Z" fill="white"/>
+                            <path fillRule="evenodd" clipRule="evenodd" d="M18.2896 11.4958C18.5701 11.7743 18.5701 12.2257 18.2896 12.5042L12.7541 18L11.7384 16.9916L16.766 12L11.7384 7.00845L12.7541 6L18.2896 11.4958Z" fill="white"/>
+                            <path fillRule="evenodd" clipRule="evenodd" d="M17.9972 12.7131H5.5V11.2869H17.9972V12.7131Z" fill="white"/>
                         </svg>
                     </button>
                 </form>
