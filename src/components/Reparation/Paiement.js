765c33fbb245661aea4cb30c5acde8bd49953388
SarahOtmane <sarahotmane02@gmail.com>
2024-06-14 15:58:33 +0200

    composant paiement ok

--- src/components/Reparation/Paiement.js
+++ src/components/Reparation/Paiement.js
@@ -1,88 +1,128 @@
 import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
-import '../../css/cart.css'
+import '../../css/cart.css';
 import Titre from '../Titre';
 
 import product from '../../assets/pictures/landingPage/product1.png';
+import visa from '../../assets/pictures/order/visa.svg';
+import amex from '../../assets/pictures/order/amex.svg';
+import cb from '../../assets/pictures/order/cb.svg';
 
-
-const Paiement = () =>{
+const Paiement = () => {
     const navigate = useNavigate();
+    const [commands, setCommands] = useState([]);
+    const [total, setTotal] = useState(0);
 
-    return(
+    useEffect(() => {
+        const getCommand = () => {
+            const command = localStorage.getItem('command');
+            if (command) {
+                const parsedCommands = JSON.parse(command);
+                setCommands(parsedCommands);
+
+                let somme = 0;
+                for (const c of parsedCommands) {
+                    somme += parseInt(c.price);
+                }
+                setTotal(somme);
+            }
+        };
+
+        getCommand();
+    }, []);
+
+    return (
         <main>
-            <Titre titre="Valider votre panier" lien="/user/cart/check" classe="backGris" />
+            <Titre titre="Paiement" lien="/user/reparation/paiement" classe="backGris" />
 
-            <form className='row'>
+            <form className='row' onSubmit={(e) => e.preventDefault()}>
                 <section className='livraison column'>
-                    <h2>Adresse de livraison</h2>
+                    <h2>Paiement</h2>
 
-                    <lable className="text_bold">Numéro de rue</lable>
-                    <input type="text" placeholder="30 Rue Solférino" />
-
-                    <lable className="text_bold">Ville</lable>
-                    <input type="text" placeholder="Boulogne-Billancourt" />
-
-                    <div className='row double justifycontent_spbetween'>
-                        <div className='column'><lable className="text_bold">Code Postal</lable>
-                        <input type="text" placeholder="92100" /></div>
-
-                        <div className='column'><lable className="text_bold">Pays</lable>
-                        <input type="text" placeholder="France" /></div>
+                    <label className="text_bold">Numéro de carte</label>
+                    <div className='numCarte'>
+                        <input type="text" />
+                        <figure className='row'>
+                            <img src={cb} alt='logo cb' />
+                            <img src={amex} alt='logo amex' />
+                            <img src={visa} alt='logo visa' />
+                        </figure>
                     </div>
 
-                    <lable className="text_bold">Email</lable>
-                    <input type="text" placeholder="test@gmail.com" />
+                    <div className='row double justifycontent_spbetween'>
+                        <div className='column'>
+                            <label className="text_bold">Date d'expiration</label>
+                            <input type="text" placeholder="MM/YY" />
+                        </div>
+
+                        <div className='column'>
+                            <label className="text_bold">CVC</label>
+                            <input type="text" placeholder="CVC" />
+                        </div>
+                    </div>
 
                     <div className='row double justifycontent_spbetween'>
-                        <div className='column'><lable className="text_bold">Nom</lable>
-                        <input type="text" placeholder="Cruz" /></div>
+                        <div className='column'>
+                            <label className="text_bold">Pays</label>
+                            <input type="text" placeholder="Pays" />
+                        </div>
 
-                        <div className='column'><lable className="text_bold">Prénom</lable>
-                        <input type="text" placeholder="Tom" /></div>
+                        <div className='column'>
+                            <label className="text_bold">Code promo</label>
+                            <input type="text" placeholder="Code promo" />
+                        </div>
                     </div>
                 </section>
 
-                <section className='commande' >
+                <section className='commande'>
                     <h2>Votre commande</h2>
                     <article className='row alignitem_center justifycontent_spbetween'>
                         <figure>
-                            <img src={product} alt='' />
-                            <img src={product} alt='' />
+                            {commands.map(command =>(
+                                <img src={product} alt='produit' key={command.id} />
+                            ))}
                         </figure>
 
-                        <button onClick={navigate('/')} >Modifier</button>
+                        <button className='white' type="button" onClick={() => navigate('/user/reparation/panier')}>
+                            Modifier
+                        </button>
                     </article>
 
                     <section className='recap'>
                         <article className='row justifycontent_spbetween'>
-                            <span className='text_bold'>Prix :</span>
-                            <span>75.00€</span>
+                            <span>Prix :</span>
+                            <span className='text_bold'>{total} €</span>
                         </article>
 
                         <article className='row justifycontent_spbetween'>
-                            <span className='text_bold'>Livraison :</span>
-                            <span>Offerte</span>
+                            <span>Livraison :</span>
+                            <span className='text_bold'>Offerte</span>
                         </article>
 
                         <article className='row justifycontent_spbetween'>
-                            <span className='text_bold'>Taxes :</span>
-                            <span>3.00€</span>
+                            <span>Taxes :</span>
+                            <span className='text_bold'>3.00 €</span>
                         </article>
 
                         <div></div>
 
                         <article className='row justifycontent_spbetween'>
-                            <span className='text_bold'>Total :</span>
-                            <span>78.00€</span>
+                            <span>Total :</span>
+                            <span className='text_bold'>{total !== 0 ? total + 3 : 0} €</span>
                         </article>
                     </section>
-
-                    <button type='submit'>Payer la commande</button>
+                    <button className="black" type="button" onClick={() => navigate('/user/reparation/paiement')}>
+                        Procéder au paiement
+                        <svg width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fillRule="evenodd" clipRule="evenodd" d="M13.2896 5.49578C13.5701 5.77425 13.5701 6.22575 13.2896 6.50422L7.75414 12L6.73841 10.9916L11.766 6L6.73841 1.00845L7.75414 0L13.2896 5.49578Z" fill="#999999"/>
+                            <path fillRule="evenodd" clipRule="evenodd" d="M12.9972 6.71308H0.5V5.28692H12.9972V6.71308Z" fill="#999999"/>
+                        </svg>
+                    </button>
                 </section>
             </form>
         </main>
-    )
-}
+    );
+};
 
 export default Paiement;
