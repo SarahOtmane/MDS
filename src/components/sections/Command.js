41d3c7283303cb30c3c18bce1cf59449b1acc950
SarahOtmane <sarahotmane02@gmail.com>
2024-06-14 08:43:52 +0200

    update input

--- src/components/sections/Command.js
+++ src/components/sections/Command.js
@@ -2,10 +2,14 @@
 
 
 
-const Command = () =>{
+const Command = ({command}) =>{
     return(
-        <article className="command">
-
+        <article className="command row justifycontent_spbetween">
+            <img src={command.picture} alt='produit' />
+            <div className="column">
+                <p>{command.categorie} : {command.clothType} </p>
+                <p>{command.besoinType} : {command.reparationType ? command.reparationType : command.broderieType} </p>
+            </div>
         </article>
     )
 }
