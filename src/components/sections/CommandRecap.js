afc473a1635f88c80b6e61bd151cb442bf8b6b38
SarahOtmane <sarahotmane02@gmail.com>
2024-06-14 08:32:39 +0200

    composant CommandRecap ok

--- src/components/sections/CommandRecap.js
+++ src/components/sections/CommandRecap.js
@@ -11,19 +11,19 @@ const CommandRecap = ({name}) =>{
                 <p>90.00 €</p>
             </article>
             <article className="row justifycontent_spbetween">
-                <p>Sous Total :</p>
-                <p>90.00 €</p>
+                <p>Dépot :</p>
+                <p>Gratuit</p>
             </article>
             <article className="row justifycontent_spbetween">
-                <p>Sous Total :</p>
-                <p>90.00 €</p>
+                <p>Taxes :</p>
+                <p>3.00 €</p>
             </article>
             <hr></hr>
             <article className="row justifycontent_spbetween">
-                <p>Sous Total :</p>
-                <p>90.00 €</p>
+                <p>Total</p>
+                <p>100.00 €</p>
             </article>
-            <button>
+            <button className="black">
                 Ajouter au panier 
                 <svg width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fillRule="evenodd" clipRule="evenodd" d="M13.2896 5.49578C13.5701 5.77425 13.5701 6.22575 13.2896 6.50422L7.75414 12L6.73841 10.9916L11.766 6L6.73841 1.00845L7.75414 0L13.2896 5.49578Z" fill="#999999"/>
