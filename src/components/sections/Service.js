dd8493f17a1f8af570fe31dc15d724bf0c4f616d
SarahOtmane <sarahotmane02@gmail.com>
2024-06-15 16:36:36 +0200

    update composant

--- src/components/sections/Service.js
+++ src/components/sections/Service.js
@@ -5,24 +5,20 @@ const Service = () =>{
     return(
         <section className="service backRose2 row">
             <h2 className='text_uppercase'>Service d'artisans</h2>
-            <div className='column'>
-                <div className='row'>
-                    <article className='column card'>
-                        <h3>Couturiers</h3>
-                        <p>Ils réparent et transforment tous vos textiles, des simples ourlets. Tout est possible, du dressing à la déco d'intérieur.</p>
-                    </article>
-                    <article className='column card'>
-                        <h3>Cordonniers</h3>
-                        <p>Ils préviennent l'usure et restaurent vos chaussures, prolongeant ainsi leur durée de vie pas à pas.</p>
-                    </article>
-                    <article className='column card'>
-                        <h3>Maroquiniers</h3>
-                        <p>Ils entretiennent, réparent et revitalisent vos sacs et accessoires en cuir, offrant tout, de la nouvelle teinture à la création sur-mesure.</p>                   
-                    </article>
-                </div>
-                <article className='row'>
+            <div className='row'>
+                <article className='column card'>
+                    <h3>Couturiers</h3>
+                    <p>Ils réparent et transforment tous vos textiles, des simples ourlets. Tout est possible, du dressing à la déco d'intérieur.</p>
                     <a href='/artisans' className='row text_bold alignitem_center'>En savoir plus <img src={arrow} alt='flèche' /> </a>
+                </article>
+                <article className='column card'>
+                    <h3>Cordonniers</h3>
+                    <p>Ils préviennent l'usure et restaurent vos chaussures, prolongeant ainsi leur durée de vie pas à pas.</p>
                     <a href='/artisans' className='row text_bold alignitem_center'>En savoir plus <img src={arrow} alt='flèche' /> </a>
+                </article>
+                <article className='column card'>
+                    <h3>Maroquiniers</h3>
+                    <p>Ils entretiennent, réparent et revitalisent vos sacs et accessoires en cuir, offrant tout, de la nouvelle teinture à la création sur-mesure.</p>                   
                     <a href='/artisans' className='row text_bold alignitem_center'>En savoir plus <img src={arrow} alt='flèche' /> </a>
                 </article>
             </div>
