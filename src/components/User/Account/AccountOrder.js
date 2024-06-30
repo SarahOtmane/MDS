9b93d116e19edaba15147f3d64dbadea1bdbd63d
SarahOtmane <sarahotmane02@gmail.com>
2024-06-30 13:24:08 +0200

    update axios path

--- src/components/User/Account/AccountOrder.js
+++ src/components/User/Account/AccountOrder.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axiosInstance from '../../../axiosConfig';
+import axiosInstance from '../../service/axiosConfig';
 import { useNavigate } from 'react-router-dom';
 import { format } from 'date-fns';
 import { fr } from 'date-fns/locale';
