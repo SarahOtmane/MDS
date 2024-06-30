c958068e8f236e733ec402f71221d74152a38213
SarahOtmane <sarahotmane02@gmail.com>
2024-06-30 13:23:59 +0200

    update axios path

--- src/components/User/Account/AccountAdress.js
+++ src/components/User/Account/AccountAdress.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axiosInstance from '../../../axiosConfig';
+import axiosInstance from '../../service/axiosConfig';
 
 import '../../../css/account.css';
 
