2080979c87182c879d50ee267d69d7a1b102aeb2
SarahOtmane <sarahotmane02@gmail.com>
2024-06-30 13:24:17 +0200

    update axios path

--- src/components/User/Sign/SignIn.js
+++ src/components/User/Sign/SignIn.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { addToken} from '../../../axiosConfig';
+import { addToken} from '../../service/axiosConfig';
 
 import '../../../css/sign.css';
 
