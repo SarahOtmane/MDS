1497f88b9f304d708473cbcf8816538498b06256
SarahOtmane <sarahotmane02@gmail.com>
2024-07-04 20:45:53 +0200

    update

--- src/components/Order/SuccefullOrder.js
+++ src/components/Order/SuccefullOrder.js
@@ -2,9 +2,12 @@ import '../../css/cart.css';
 import Titre from "../Titre";
 
 import success from '../../assets/pictures/order/succefullOrder.png'
+import { useNavigate } from 'react-router-dom';
 
 
 const SuccefullOrder = () =>{
+    const navigate = useNavigate();
+
     return(
         <main>
             <Titre titre="Commande réussie" lien="/user/order/success" classe="backVert" />
@@ -19,7 +22,7 @@ const SuccefullOrder = () =>{
                         <li>Étape 3 : Récupérez-le transformé et revitalisé</li>
                     </ul>
                 </article>
-                <button>
+                <button onClick={navigate('user/my-account/order')} >
                     Accéder à mon Compte 
                     <svg width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path fillRule="evenodd" clipRule="evenodd" d="M13.2896 5.49578C13.5701 5.77425 13.5701 6.22575 13.2896 6.50422L7.75414 12L6.73841 10.9916L11.766 6L6.73841 1.00845L7.75414 0L13.2896 5.49578Z" fill="white"/>
