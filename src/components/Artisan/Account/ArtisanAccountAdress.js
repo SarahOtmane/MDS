8dd1cefecbd834c9b701bfff5e5cf9718d97a9b8
SarahOtmane <sarahotmane02@gmail.com>
2024-07-04 15:54:12 +0200

    upfate  ArtisanAccountAdress

--- src/components/Artisan/Account/ArtisanAccountAdress.js
+++ src/components/Artisan/Account/ArtisanAccountAdress.js
@@ -18,26 +18,10 @@ const ArtisanAccountAdress = () =>{
     useEffect(() => {
         const getArtisan = async () => {
             try {
-                const response = await axiosInstance.get('/artisans');
+                const response = await axiosInstance.get('/persons/artisan');
                 setArtisan(response.data);
             } catch (error) {
-                const status = error.response ? error.response.status : 500;
-                switch (status) {
-                    case 401:
-                        navigate('/error401');
-                        break;
-                    case 403:
-                        navigate('/error403');
-                        break;
-                    case 404:
-                        navigate('/error404');
-                        break;
-                    case 500:
-                        navigate('/error500');
-                        break;
-                    default:
-                        console.error('Erreur lors de l\'enregistrement de l\'utilisateur:', error);
-                }  
+                console.error('Erreur lors de l\'enregistrement de l\'utilisateur:', error);
             }
         };
 
@@ -56,26 +40,10 @@ const ArtisanAccountAdress = () =>{
         e.preventDefault();
 
         try {
-            await axiosInstance.put('artisans', artisan);
+            await axiosInstance.put('/persons/artisan', artisan);
             setUpdate(true);
         } catch (error) {
-            const status = error.response ? error.response.status : 500;
-            switch (status) {
-                case 401:
-                    navigate('/error401');
-                    break;
-                case 403:
-                    navigate('/error403');
-                    break;
-                case 404:
-                    navigate('/error404');
-                    break;
-                case 500:
-                    navigate('/error500');
-                    break;
-                default:
-                    console.error('Erreur lors de l\'enregistrement de l\'utilisateur:', error);
-            }  
+            console.error('Erreur lors de l\'enregistrement de l\'utilisateur:', error);
         }
     };
 
@@ -98,9 +66,9 @@ const ArtisanAccountAdress = () =>{
                         <label className="text_bold">Numéro de la rue</label>
                         <input 
                             type="text" 
-                            name="streetAdress"
+                            name="streetAddress"
                             placeholder="4 Rue Solférino" 
-                            defaultValue={artisan.streetAdress}
+                            defaultValue={artisan.streetAddress}
                             onChange={updateChamps}
                             required
                         />
