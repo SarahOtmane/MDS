2369d736c2e39c45eb155954ece153e9133c3ce8
SarahOtmane <sarahotmane02@gmail.com>
2024-07-24 22:50:24 +0200

    update url

--- src/service/axiosConfig.js
+++ src/service/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://api.renowear.fr/'
+    baseURL: 'https://api.renowear.fr/'
 });
 
 instance.interceptors.request.use(config => {
