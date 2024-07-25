646355b122f8d6bf31b531fae0576977d85e394b
SarahOtmane <sarahotmane02@gmail.com>
2024-07-25 10:59:14 +0200

    upate

--- src/components/Order/SuccefullOrder.js
+++ src/components/Order/SuccefullOrder.js
@@ -22,7 +22,7 @@ const SuccefullOrder = () =>{
                         <li>Étape 3 : Récupérez-le transformé et revitalisé</li>
                     </ul>
                 </article>
-                <button onClick={navigate('user/my-account/order')} >
+                <button onClick={navigate('/user/my-account/order')} >
                     Accéder à mon Compte 
                     <svg width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path fillRule="evenodd" clipRule="evenodd" d="M13.2896 5.49578C13.5701 5.77425 13.5701 6.22575 13.2896 6.50422L7.75414 12L6.73841 10.9916L11.766 6L6.73841 1.00845L7.75414 0L13.2896 5.49578Z" fill="white"/>
